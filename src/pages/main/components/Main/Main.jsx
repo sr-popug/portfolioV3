21dd94bfbed17de67bea303f0fb9b151b1833bfe
sr-popug <antonpower2008@gmail.com>
2024-11-30 16:37:39 +0300

    добавление neduca

--- src/pages/main/components/Main/Main.jsx
+++ src/pages/main/components/Main/Main.jsx
@@ -4,34 +4,42 @@ import backgroundImg from './images/фон.jpg'
 import './main.less'
 
 function Main() {
-	useGSAP(() => {
-		const tl = gsap.timeline({ delay: 0.5 })
-		tl.fromTo(
-			'.spanchik',
-			{ opacity: 0, y: 100 },
-			{ opacity: 1, duration: 0.4, y: 0, stagger: 0.3, ease: 'power3.inOut' }
-		)
-	})
-	return (
-		<article className='main-mainpage'>
-			<article className='background'>
-				<img src={backgroundImg} alt='img' />
-			</article>
+  useGSAP(() => {
+    const tl = gsap.timeline({ delay: 0.5 })
+    tl.fromTo(
+      '.spanchik',
+      { opacity: 0, y: 100 },
+      { opacity: 1, duration: 0.4, y: 0, stagger: 0.3, ease: 'power3.inOut' }
+    )
+  })
+  return (
+    <article className='main-mainpage'>
+      <article className='background'>
+        <img src={backgroundImg} alt='img' />
+      </article>
 
-			<article className='content'>
-				<h1 className=''>
-					<span className='name spanchik'>Антон Цуканов </span>
-					<span className='spanchik spanchik2'>
-						<span className='blue '>фронтенд</span> разработчик. Создаю цифровые{' '}
-						<a href='#works' className='orange '>
-							шедевры
-						</a>
-						.
-					</span>
-				</h1>
-			</article>
-		</article>
-	)
+      <article className='content'>
+        <h1 className=''>
+          <span className='name spanchik'>Антон Цуканов </span>
+          <span className='spanchik spanchik2'>
+            <span className='blue '>фронтенд</span> разработчик. Создаю цифровые{' '}
+            <a href='#works' className='orange '>
+              шедевры
+            </a>
+            .
+          </span>
+        </h1>
+      </article>
+      <a
+        href='../../../../../public/Резюме_Антона_Цуканова.pdf'
+        target='_blank'
+        rel='noopener noreferrer'
+        className='resume'
+      >
+        Моё резюме ➞
+      </a>
+    </article>
+  )
 }
 
 export default Main
